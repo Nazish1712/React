e3b782601527d5fdcc18830062df61c2aaf95322
Nazish Khan <nazish1712@gmail.com>
2026-02-19 15:50:01 +0530

    started with routing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import { createBrowserRouter,  RouterProvider} from "react-router-dom";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { createBrowserRouter,  Outlet, RouterProvider} from "react-router-dom";
 
 
 const AppLayout = () => {
     return(
         <div className="app">
           <Header/>
-          <Body/>
+          <Outlet/>
         </div>
     )
 }
@@ -20,16 +22,28 @@ const appRouter = createBrowserRouter(
  [
   {
     path:"/",
-    element: <AppLayout/>
+    element: <AppLayout/>,
+    children:[
+      {
+        path: "/",
+        element:<Body/>
+      },
+      {
+        path:"/about",
+        element: <About/>
+      },
+      {
+        path:"/contact",
+        element: <Contact/>
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu/>
+      }
+    ],
+    errorElement:<Error/>
   },
-  {
-    path:"/about",
-    element: <About/>
-  },
-  {
-    path:"/contact",
-    element: <Contact/>
-  }
+  
  ]
 )
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import{ LOGO_URL } from "../utils/constants"
-import {useState , useEffect} from "react"
+import {useState} from "react"
+import {Link} from "react-router-dom"
 
 
 const Header = () => {
 
 const [btnNameReact , setBtnNameReact] = useState("Login")
 
-useEffect(()=>{}, [])
 
     return(
         <div className="header">
@@ -16,9 +16,9 @@ useEffect(()=>{}, [])
            </div>
            <div className="nav-items">
             <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="contact">Contact Us</Link></li>
                 <li>Cart</li>
                 <button className="login-button" onClick={() => {
                     btnNameReact === "Login" ? 
