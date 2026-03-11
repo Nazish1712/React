bd3bc79490f4b800ebdba9d8f77dcd0041f4cca9
Nazish1712 <nazish1712@gmail.com>
2026-03-11 19:02:43 +0530

    created ItemList component

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,7 @@
 import useRestaurantMenu from "../utils/useRestaurantMenu"
 import Shimmer from "./Shimer"
 import {useParams} from "react-router-dom"
-
+import RestaurantCategory from "./RestaurantCategory"
 
 const RestaurantMenu  = () => {
     
@@ -18,16 +18,16 @@ const RestaurantMenu  = () => {
     const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card ;
     
     const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => c.card?.card?.["@type"]=== "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
+
+    console.log(categories)
      return (
-        <div className="menu">
-        <h1>{name}</h1>
-        <h3>{cuisines.join(" , ")}</h3>
-        <h3>{costForTwoMessage}</h3>
-        <ul>
-            {itemCards.map((item) => {
-             return <li key={item.card.info.id}>{item.card.info.name} - {"Rs."} {item.card.info.price/100}</li>
-            })}
-        </ul>
+        <div className="text-center">
+        <h1 className="font-bold my-6 text-2xl">{name}</h1>
+        <p className="font-bold text-lg">{cuisines.join(" , ")} - {costForTwoMessage}</p>
+    
+        {/**categories accordion */}
+        {categories.map((category) => {return (<RestaurantCategory data={category?.card?.card}/>)})}
+        
     </div>
    )
 }
